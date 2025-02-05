8fa170df09fa678f8b6349d86aedb3ec38520b32
pooYAYooq <pooyayooq@gmail.com>
2025-02-06 01:07:10 +1100

    feat: implement Hero component with responsive styles and enhance Navbar styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,17 @@
-
+import styles from './Hero.module.scss';
 const Hero = () => {
   return (
-    <h2>Hero</h2>
-  )
-}
-export default Hero
+    <div className={styles.heroContainer}>
+      <div className={styles.heroCenter}>
+        <h1>
+          manage any content <br /> anywhere
+        </h1>
+        <p>
+          Strapi is the leading open-source headless CMS. It’s 100% JavaScript
+          and fully customizable.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Hero;
