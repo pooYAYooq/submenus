3552cdf5e0dc24ac1b6c0936c8896122391187c7
pooYAYooq <pooyayooq@gmail.com>
2025-02-06 12:17:23 +1100

    feat: implement Sidebar component with responsive styles and sublinks navigation

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,46 @@
+import { FaTimes } from 'react-icons/fa';
+import sublinks from '../../data';
+import { useGlobalContext } from '../../context';
+import styles from './Sidebar.module.scss';
+
 const Sidebar = () => {
-  return <h2>Sidebar</h2>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
+  return (
+    <aside
+      className={
+        isSidebarOpen
+          ? `${styles.sidebar} ${styles.showSidebar}`
+          : styles.sidebar
+      }
+    >
+      <div className={styles.sidebarContainer}>
+        <button className={styles.closeBtn} onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className={styles.sidebarLinks}>
+          {sublinks.map((item) => {
+            const { pageId, page, links } = item;
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className={styles.sidebarSublinks}>
+                  {links.map((link) => {
+                    const { id, label, icon, url } = link;
+                    return (
+                      <a href={url} key={id}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
 };
 export default Sidebar;
